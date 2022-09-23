962dcf1f7fef72ef491700e585e58ce6fa27a3b5
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-09-23 15:57:54 -0500

    WIP

---
+++ RoboReviewer/RoboReviewer/ExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoboReviewer
+{
+    internal static class ExtensionMethods
+    {
+        internal static bool IsTooDeep(this SyntaxNode node)
+        {
+            bool isInsideFunction = false;
+            int depth = 0;
+
+            while (!(node is MethodDeclarationSyntax))
+            {
+                if (node.Parent is MethodDeclarationSyntax)
+                {
+                    isInsideFunction = true;
+                }
+
+                node = node.Parent;
+                depth++;
+            }
+
+            return isInsideFunction && depth > 2;
+        }
+
+        private static bool IsDeclaration(this SyntaxNode node)
+        {
+            SyntaxKind syntaxKind = node.Kind();
+
+            return syntaxKind == SyntaxKind.NamespaceDeclaration ||
+                   syntaxKind == SyntaxKind.ClassDeclaration ||
+                   syntaxKind == SyntaxKind.MethodDeclaration;
+        }
+
+        private static bool IsNotDeclaration(this SyntaxNode node)
+        {
+            return !node.IsDeclaration();
+        }
+    }
+}
--- RoboReviewer/RoboReviewer/RoboReviewerAnalyzer.cs
+++ RoboReviewer/RoboReviewer/RoboReviewerAnalyzer.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Data;
 using System.Linq;
-using System.Threading;
 
 namespace RoboReviewer
 {
@@ -45,48 +40,26 @@ namespace RoboReviewer
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-
             context.RegisterSyntaxTreeAction(syntaxTreeContext =>
             {
                 var root = 
                     syntaxTreeContext.Tree.GetRoot(syntaxTreeContext.CancellationToken);
 
-                foreach (var statement in root.DescendantNodes().OfType<StatementSyntax>())
-                {
-                    if (statement is BlockSyntax)
-                    {
-                        continue;
-                    }
+                var statements = 
+                    root.DescendantNodes().OfType<StatementSyntax>();
 
-                    if (statement.Parent is StatementSyntax &&
-                        !(statement.Parent is BlockSyntax))
+                foreach (var statement in statements.Where(s => (s is BlockSyntax)))
+                {
+                    if (statement.IsTooDeep())
                     {
                         syntaxTreeContext
                             .ReportDiagnostic(
                                 Diagnostic.Create(
-                                    s_rule, 
+                                    s_rule,
                                     statement.GetFirstToken().GetLocation()));
                     }
                 }
             });
-        }
-
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(s_rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
         }
     }
 }
